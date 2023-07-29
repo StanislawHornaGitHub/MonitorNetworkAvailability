63e48ee139a76f93b9ea06cb413a0069d788eede
Stanislaw Horna (HP) <stanislawhorna@outlook.com>
2023-07-29 13:42:30 +0200

    modified:   Config.psm1 	deleted:    Main.ps1 	new file:   Monitor_Network_Availability.ps1.ps1

--- Config.psm1
+++ Config.psm1
@@ -1,8 +1,17 @@
 class Configuration {
-    static [string] $logName = "Network Availability" # Default: "Network Availability"
-    static [string] $serverNameToPing = "google.com" # Default: "google.com"
-    static [string] $ConnectionFailed_popupTitle = "Connection Lost"
-    static [string] $ConnectionFailed_popupDescription = "Connection Lost" # Default: "Connection Lost"
+    static [int] $averageLatencyThresholdToDisplayNotification = 10 # Default: 200
+    static [int] $numberOfPingsToCalculateAverage = 5 # Default 10
     static [int] $delayBetweenPingsInSeconds = 5 # Default: 5
     static [int] $numberOfFailedPingsToDisplayNotification = 5 # Default: 5   
+    static [string] $serverNameToPing = "google.com" # Default: "google.com"
+    static [int] $delayBetweenSameNotificationsInSeconds = 20 # Default: 20
+
+    static [bool] $logEnabled = $true # Default: $true
+    static [string] $logName = "Network Availability"
+
+    static [string] $ConnectionFailed_popupTitle = "Connection Lost"
+    static [string] $ConnectionFailed_popupDescription = "Connection to host $([Configuration]::serverNameToPing) is lost"
+    static [string] $HighLatencyWarining_pop_popupTitle = "High ping latency"
+    static [string] $HighLatencyWarining_popupDescription = "Average latency of last $([Configuration]::numberOfPingsToCalculateAverage) pings is greater than $([Configuration]::averageLatencyThresholdToDisplayNotification) ms"
+
 }
